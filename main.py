c22c30c0f6f350d138e51362e37e09b0eac9491a
Josh Welicky <joshwelicky@gmail.com>
2025-12-01 18:07:01 -0600

    New merger that allows one or the other. Also included output images for quick reference

--- main.py
+++ main.py
@@ -12,20 +12,23 @@ def main():
     parser.add_argument('mark_text')
     args = parser.parse_args()
 
-    if args.mark_text not in ['0', '1']:
-        raise Exception("mark_text arg must be 0 or 1")
+    if args.mark_text not in ['0', '1', '2']:
+        raise Exception("mark_text arg must be 0, 1, or 2")
 
 
     if os.path.dirname(args.output):
         os.makedirs(os.path.dirname(args.output), exist_ok=True)
 
 
-    marked_pins = PinDetector.mark_pins(args.input)
+    #marked_pins = PinDetector.mark_pins(args.input)
     
     if args.mark_text == "0":
-        output_img = marked_pins
+        output_img = PinDetector.mark_pins(args.input)
+    elif args.mark_text == "1":
+        img = cv2.imread(args.input)
+        output_img = read_ic_text.read_ic_text(img)
     else:
-        output_img = read_ic_text.read_ic_text(marked_pins)
+        output_img = read_ic_text.read_ic_text(PinDetector.mark_pins(args.input))
     
     
     cv2.imwrite(args.output, output_img)
